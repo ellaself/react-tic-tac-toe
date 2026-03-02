2f929fb7728701f1e6d6a133b8bdb2e28d38b788
ellaself <ellaself@hawaii.edu>
2026-03-01 15:27:01 -1000

    Task 6

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -24,15 +24,21 @@ function Board({ xIsNext, squares, onPlay }) {
 
   const winner = declearWinner(squares);
   let status;
+ 
   if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
+    status = 'Winner: ' + winner ;
+  } else { 
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
     <>
+    {/* added lil comment */}
       <div className="status">{status}</div>
+      {winner && (
+    <p className="congrats"> Congratulations!! You Win!</p>
+      )}
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
